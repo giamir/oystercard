6deb538cb9bd8a2d755bc06656048d9ba870f62b
Daniela <daniela.galeote@gmail.com>
2015-12-01 10:55:39 +0000

    added max balance using constant

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,20 @@
 class Oystercard
   attr_reader :balance
+  MAX_BALANCE = 90
 
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
+    fail "exceeded max balance of #{MAX_BALANCE}" if exceed_limit?(amount)
     @balance += amount
   end
+
+private
+
+def exceed_limit?(amount)
+  balance + amount > MAX_BALANCE
 end
+
+end 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -13,5 +13,10 @@ describe Oystercard do
     it 'can top up the balance' do
       expect { oystercard.top_up 1 }.to change { oystercard.balance }.by 1
     end
+    it 'fails if new balance exceeds max balance' do
+      max = described_class::MAX_BALANCE
+      oystercard.top_up max
+      expect { oystercard.top_up 1}.to raise_error "exceeded max balance of #{max}"
+    end
   end
 end
