baaad3689f3917df2ac5a8eb6bcaca021482fe39
Daniela <daniela.galeote@gmail.com>
2015-12-01 16:10:32 +0000

    including zone to each station

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,5 +1,12 @@
 require 'station'
 
 describe Station do
+  subject(:station) { described_class.new(name: 'Aldgate East', zone: 3) }
 
+  it 'gives you the name of the station' do
+    expect(station.name).to eq 'Aldgate East'
+  end
+  it 'gives you the zone of the station' do
+    expect(station.zone).to eq 3
+  end
 end
